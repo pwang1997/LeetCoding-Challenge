d29d8d5738a5375dceea19cae7f0265377106ac6
pwang <37675715+pwang1997@users.noreply.github.com>
2021-04-06 13:35:32 -0400

    added day 5 and 6 daily question answer

---
+++ april-2021/week1/Minimum Operations to Make Array Equal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int n) {
+        int sum = 0;
+        int rounds = n / 2;
+        while(rounds > 0) {
+            sum += (n-rounds*2+1);
+            rounds--;
+        }
+        return sum;
+    }
+}

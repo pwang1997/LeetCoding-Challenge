df19d7b47fa91624e5e0c2a5883e950e5bf323a3
pwang <37675715+pwang1997@users.noreply.github.com>
2021-04-08 00:32:01 -0400

    added day 7 challenge answer

---
+++ april-2021/week1/Determine if String Halves Are Alike.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int n = s.length();
+
+        String vowels = "aeiouAEIOU";
+
+        int aVowelCount = 0;
+        for (int i = 0; i < n / 2; i++) {
+            if (vowels.indexOf(s.charAt(i)) != -1) {
+                aVowelCount++;
+            }
+        }
+
+        int bVowelCount = 0;
+        for (int i = n / 2; i < n; i++) {
+            if (vowels.indexOf(s.charAt(i)) != -1) {
+                bVowelCount++;
+            }
+        }
+
+        return aVowelCount == bVowelCount;
+    }
+}
